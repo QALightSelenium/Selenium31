0818022cfe76fe4273ca25dbeed9aee9c05b69da
QALightSelenium <36269563+QALightSelenium@users.noreply.github.com>
2018-02-11 11:26:08 +0200

    Add loop

--- DoroshProj/src/main/java/Selenium31/App.java
+++ DoroshProj/src/main/java/Selenium31/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        for(int i = 0; i < 5; i++){
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        }
     }
 }
