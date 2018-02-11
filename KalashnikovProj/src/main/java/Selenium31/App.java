ab3e58252030fa9f51f7bf8a5ef018a231e1aaf5
QALightSelenium <36269563+QALightSelenium@users.noreply.github.com>
2018-02-11 11:43:10 +0200

    Update App.java

--- KalashnikovProj/src/main/java/Selenium31/App.java
+++ KalashnikovProj/src/main/java/Selenium31/App.java
@@ -7,9 +7,9 @@ package Selenium31;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {for(int i = 0; i < 0; i++){
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+    }
     }
 
 }
